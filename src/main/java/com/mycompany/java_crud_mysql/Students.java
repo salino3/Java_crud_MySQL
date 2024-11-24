07e8d74f8c7b52186d6fb84c7869f92ea37ec98c
Salino3 <flaviobcn012@gmail.com>
2024-11-24 11:08:20 +0100

    add variables to class Student

--- src/main/java/com/mycompany/java_crud_mysql/Students.java
+++ src/main/java/com/mycompany/java_crud_mysql/Students.java
@@ -10,4 +10,35 @@ package com.mycompany.java_crud_mysql;
  */
 public class Students {
     
+       int code;
+    String name;
+    String surnames;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurnames() {
+        return surnames;
+    }
+
+    public void setSurnames(String surnames) {
+        this.surnames = surnames;
+    }
+    
+ 
+    
+    
 }
